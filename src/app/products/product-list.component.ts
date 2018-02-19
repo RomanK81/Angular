76f239c50a8e3e66f4be23ccf3ad5709a662d84e
Roman Kirzner <Roman.Kirzner@sarine.com>
2018-02-19 09:27:39 +0200

    add on onit

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,7 +6,7 @@ import {IProduct} from './product';
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
